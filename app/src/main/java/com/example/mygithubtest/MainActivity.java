113875a94d880b724a996585234fbe75389fb273
dungmau <dungmauchen@gmail.com>
2020-11-26 00:42:25 +0800

    v1

--- app/src/main/java/com/example/mygithubtest/MainActivity.java
+++ app/src/main/java/com/example/mygithubtest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("brad", "ok");
+        Log.v("brad", "ok v1");
     }
 }
